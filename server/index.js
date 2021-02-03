5e6673d1ceb8adb19d4b3d8fb59aacbe72f5b7ca
SaneelDaniel <saneel.daniel07@gmail.com>
2021-02-02 22:16:53 -0800

    first commit

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const fetch = require("node-fetch");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(cors());
+
+const search = "REST API";
+
+app.get("/videos", (req, res) => {
+  const url = `https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=50&q=${search}`;
+  fetch(`${url}&key=${process.env.GOOGLE_YOUTUBE_API_KEY}`)
+    .then((response) => response.json())
+    .then((json) => {
+      res.json(json.items);
+      console.log(`${url}&key=${process.env.GOOGLE_YOUTUBE_API_KEY}`);
+    });
+});
+
+function notFound(req, res, next) {
+  res.status(404);
+  const err = new Error("Not Found");
+  next(err);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("Listening on port", port);
+});
