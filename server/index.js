7297c4253c1e8f05675eeff2eaaa38f0dd557f34
SaneelDaniel <saneel.daniel07@gmail.com>
2021-02-09 12:28:18 -0800

    first push

--- server/index.js
+++ server/index.js
@@ -10,18 +10,35 @@ const app = express();
 app.use(morgan("tiny"));
 app.use(cors());
 
-const search = "REST API";
+const search = null;
+const playList = "";
+const defaultSearchNumberValue = 25;
 
 app.get("/videos", (req, res) => {
-  const url = `https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=50&q=${search}`;
-  fetch(`${url}&key=AIzaSyAnGjpYri0Pvqn50NGLgbF8u1soUCpa_8c`)
+  const url = `https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=${
+    req.query.maxResults ? req.query.maxResults : defaultSearchNumberValue
+  }&q=${req.query.q !== "" ? req.query.q : "youtube"}&pageToken=${
+    req.query.pageToken ? req.query.pageToken : ""
+  }`;
+  console.log("query: >>>>>>", req.query);
+
+  fetch(`${url}&key=AIzaSyBY9-sSv1E12YojjpF5OsLY8C_bZ7VcT5Y`)
     .then((response) => response.json())
     .then((json) => {
+      console.log("URL: >>>>>>", url);
+
+      console.log("JSON: >>>>>>", json);
       res.json(json.items);
-      console.log(`${url}&key=${process.env.GOOGLE_YOUTUBE_API_KEY}`);
+      console.log("JSON.ITEMS: >>>>>>", json);
     });
 });
 
+app.get("/watchVideo", (req, res) => {
+  const url = "https://www.youtube.com/watch?v=kXYiU_JCYtU&feature=youtu.be";
+  fetch(url)
+    .then((response) => response.json)
+    .then();
+});
 function notFound(req, res, next) {
   res.status(404);
   const err = new Error("Not Found");
